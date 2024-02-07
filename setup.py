a481d9619ca07664139f230197949d9629d775ed
kamilanindita <kamilanindita@gamil.com>
2024-02-07 13:20:27 +0700

    upgrade to django 4.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='django32-datatable',
+    name='django42-datatable',
     version='0.3.2',
     author='shymonk',
     author_email='hellojohn201@gmail.com',
